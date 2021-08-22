f15f8d3f9dbd4387fa884c16e6cadf20442f314e
Kenia Balestra <kenia.balestra@gmail.com>
2021-08-22 21:36:08 +0000

    Initial commit with all my work so far Aug 22

---
+++ string-length.js
@@ -0,0 +1,8 @@
+let example = 'example string'
+let tamanho_da_variavel = example.length
+console.log(tamanho_da_variavel)
+
+let example = 'example string'
+console.log(example.length)
+
+console.log('example string'.length)
