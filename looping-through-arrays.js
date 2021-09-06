7fc4983909bed70ccd52a5ec16fcd3d36f74375e
Kenia Balestra <kenia.balestra@gmail.com>
2021-09-06 21:17:53 +0000

    Objects and Functions from Programming Intro

--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -1,6 +1,5 @@
-
-var pets = ['cat', 'dog', 'rat']
-for (var i = 0; i < pets.length; i++) {
+const pets = ['cat', 'dog', 'rat']
+for (let i = 0; i < pets.length; i++) {
 pets[i] = pets[i] + 's'
 }
 console.log(pets);
